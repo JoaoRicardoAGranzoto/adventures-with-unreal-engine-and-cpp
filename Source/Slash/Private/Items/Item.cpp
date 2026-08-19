0a157b2ca113f2798940322ad21dea802a50f370
Joao Ricardo Araujo Granzoto <joaoricardo.granzoto@gmail.com>
2026-08-19 01:33:37 -0300

    debugging the actor

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -10,9 +10,21 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("AItem::BeginPlay()"));
 }
 
 void AItem::Tick(float DeltaTime)
 {
+	
+	
+	
+	
 	Super::Tick(DeltaTime);
+
+	if (GetWorld())
+	{
+		DrawDebugSphere(GetWorld(), GetActorLocation(), 100.f, 10, FColor::Red);
+		DrawDebugLine(GetWorld(), GetActorLocation(), GetActorForwardVector() * 1000.f, FColor::Red);
+	}
 }
